abedefb39432dd1710045100a02dbeb2bf397fed
Nerwin Hernando <nerwinc.hernando@gmail.com>
2018-10-26 23:48:11 +0800

    render header

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
 import React from 'react';
 import { View, Text } from 'react-native';
-
+import { Header } from './components/common/Header';
 class App extends React.Component {
     render() {
         return(
             <View>
-                <Text>Hi</Text>
-                <Text>Hi</Text>
-                <Text>Hi</Text>
-                <Text>Hi</Text>
+                <Header headerText='Authentication' />
                 <Text>Hi</Text>
             </View>
         );
