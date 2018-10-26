d12d23c4ca389f3402c2648e5fab2c6ffe5bf03b
Nerwin Hernando <nerwinc.hernando@gmail.com>
2018-10-26 20:44:43 +0800

    new app file

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+class App extends React.Component {
+    render() {
+        return(
+            <View>
+                <Text>Hi</Text>
+                <Text>Hi</Text>
+                <Text>Hi</Text>
+                <Text>Hi</Text>
+                <Text>Hi</Text>
+            </View>
+        );
+    }
+};
+
+export default App;
