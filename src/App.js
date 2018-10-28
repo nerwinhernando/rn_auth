e5918ef629724c101651a98e7f816f9629595027
Nerwin Hernando <nerwinc.hernando@gmail.com>
2018-10-28 15:40:33 +0800

    Firebase auth events and spiner

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import { View } from 'react-native';
 import firebase from 'firebase';
-import { Header } from './components/common';
+import { Header, Button, Spinner } from './components/common';
 import LoginForm from './components/LoginForm';
 class App extends React.Component {
+    state = { loggedIn: null }
+
     componentWillMount() {
         firebase.initializeApp({
             apiKey: "AIzaSyCXPoO-4iCwWbmGbd5cVSqmxY4AfxtE7cU",
@@ -13,13 +15,32 @@ class App extends React.Component {
             storageBucket: "auth-2b228.appspot.com",
             messagingSenderId: "71520933884"
         });
+
+        firebase.auth().onAuthStateChanged((user) => {
+            if (user) {
+                this.setState({ loggedIn: true });
+            } else {
+                this.setState({ loggedIn: false });
+            }
+        });
+    }
+
+    renderContent() {
+        switch(this.state.loggedIn) {
+            case true:
+                return(<Button>Log Out</Button>);
+            case false:
+                return <LoginForm />
+            default:
+                return <Spinner size="large" />
+        }
     }
 
     render() {
         return(
             <View>
                 <Header headerText='Authentication' />
-                <LoginForm />
+                {this.renderContent()}
             </View>
         );
     }
