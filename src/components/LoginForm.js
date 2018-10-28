913327c5b42cb32992f2cf30009aa3229b37fe60
Nerwin Hernando <nerwinc.hernando@gmail.com>
2018-10-28 14:24:34 +0800

    Add style for Input

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -2,13 +2,14 @@ import React from 'react';
 import { Button, Card, CardSection, Input } from './common';
 
 class LoginForm extends React.Component {
-    state = { text: 'Provide input' }
+    state = { text: 'Enter something' }
     
     render() {
         return(
             <Card>
                 <CardSection>
                     <Input
+                        label='Email'
                         value={this.state.text} 
                         onChangeText={text => this.setState({ text })}
                     />
--- src/components/common/Input.js
+++ src/components/common/Input.js
@@ -2,16 +2,39 @@ import React from 'react';
 import { View, TextInput, Text } from 'react-native';
 
 const Input = ({ label, value, onChangeText }) => {
+    const { inputStyle, labelStyle, constainerStyle } = styles;
     return (
-        <View>
-            <Text>{label}</Text>
+        <View style={constainerStyle}>
+            <Text style={labelStyle}>{label}</Text>
             <TextInput
                 value={value}
                 onChangeText={onChangeText} 
-                style={{ height: 20, width: 100 }}
+                style={inputStyle}
             />
         </View>
     );
+};
+
+const styles = {
+    inputStyle: {
+        color: '#000',
+        paddingLeft: 5,
+        paddingRight: 5,
+        fontSize: 18,
+        lineHeight: 23,
+        flex: 2
+    },
+    labelStyle: {
+        fontSize: 18,
+        paddingLeft: 20,
+        flex: 1
+    },
+    constainerStyle: {
+        height: 40,
+        flex: 1,
+        flexDirection: 'row',
+        alignItems: 'center'
+    }
 };
 
 export { Input };
